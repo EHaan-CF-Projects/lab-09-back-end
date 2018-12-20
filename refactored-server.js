8ba77ac0da56345a4be678d0e3213ec3bdf67945
EvyHaan <evy.haan@gmail.com>
2018-12-20 10:41:44 -0800

    fully refactored, bugs to fix

--- refactored-server.js
+++ refactored-server.js
@@ -34,6 +34,10 @@ function handleError(err, res) {
 // API Routes
 app.get('/location', getLocation);
 app.get('/weather', getWeather);
+app.get('/yelp', getYelp);
+app.get('/movies', getMovies);
+app.get('/meetups', getMeetups);
+app.get('/trails', getTrails);
 
 //===============================
 
@@ -67,7 +71,6 @@ function Location(query, res) {
 Location.lookupLocation = (location) => {
   const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
   const values = [location.query];
-
   return client.query(SQL, values)
     .then(result => {
       if(result.rowCount > 0) {
@@ -83,7 +86,6 @@ Location.prototype = {
   save: function() {
     const SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;`;
     const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
-
     return client.query(SQL, values)
       .then(result => {
         this.id = result.rows[0].id;
@@ -95,16 +97,12 @@ Location.prototype = {
 function getLocation(request, response) {
   Location.lookupLocation({
     tableName: Location.tableName,
-
     query: request.query.data,
-
     cacheHit: function(result) {
       response.send(result.rows[0]);
     },
-
     cacheMiss: function() {
       const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.query}&key=${process.env.GEOCODE_API_KEY}`;
-
       return superagent.get(url)
         .then(result => {
           const location = new Location(this.query, result);
@@ -142,10 +140,8 @@ function getWeather(request, response) {
     cacheHit: function (result) {
       response.send(result.rows);
     },
-
     cacheMiss: function () {
       const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${req.query.data.latitude},${req.query.data.longitude}`;
-
       superagent.get(url)
         .then(result => {
           const weatherSummaries = result.body.daily.data.map(day => {
@@ -158,8 +154,7 @@ function getWeather(request, response) {
         .catch(error => handleError(error, response));
     }
   };
-  
-  Weather.lookup(weatherHandler)
+  Weather.lookup(weatherHandler);
 }
 
 //===============YELP==============================
@@ -196,7 +191,7 @@ function getYelp(request, response) {
       superagent.get(URL)
         .set('Authorization', `Bearer ${process.env.YELP_API}`)
         .then(result => {
-          const yelpSummary = result.body.businesses.map(business => {
+          const yelpSummaries = result.body.businesses.map(review => {
             const summary = new Yelp(review);
             summary.save(request.query.data.id);
             return summary;
@@ -207,4 +202,147 @@ function getYelp(request, response) {
     }
   };
   Yelp.lookup(yelpHandler);
+}
+
+//===============TMDB==============================
+function Movie(localMovie) {
+  this.tableName = 'movies';
+  this.title = localMovie.title;
+  this.overview = localMovie.overview;
+  this.average_votes = localMovie.vote_average;
+  this.total_votes = localMovie.vote_count;
+  this.image_url = 'https://image.tmdb.org/t/p/w200_and_h300_bestv2/' + localMovie.poster_path;
+  this.popularity = localMovie.popularity;
+  this.released_on = localMovie.release_date;
+}
+
+Movie.tableName = 'movies';
+Movie.lookup = lookup;
+
+Movie.prototype = {
+  save: function(location_id) {
+    const SQL = `INSERT INTO ${this.tableName} (title, overview, average_votes, total_votes, image_url, popularity, released_on, location_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)`;
+    const values = [this.title, this.overview, this.average_votes, this.total_votes, this.image_url, this.popularity, this.released_on, req.query.data.id];
+    client.query(SQL, values);
+  }
+}
+
+function getMovies(request, response) {
+  const movieHandler = {
+    tableName: Movie.tableName,
+    location: request.query.data.id,
+    cacheHit: function (result) {
+      response.send(result.rows);
+    },
+    cacheMiss: function () {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_DB_API}&query=${req.query.data.short_name}`;
+      superagent.get(url)
+        .then(result => {
+          const movieSummaries = result.body.results.map(localMovie => {
+            const summary = new Movie(localMovie);
+            summary.save(request.query.data.id);
+            return summary;
+          });
+          response.send(movieSummaries);
+        })
+        .catch(error => handleError(error, response));
+    }
+  };
+  Movie.lookup(movieHandler);
+}
+
+//===============Meetups==============================
+function Meetup(upcomingMeetup) {
+  this.tableName = 'meetups'
+  this.link = upcomingMeetup.link;
+  this.name = upcomingMeetup.name;
+  this.creation_date = new Date(upcomingMeetup.group.created).toDateString();
+  this.host = upcomingMeetup.group.name;
+}
+
+Meetup.tablename = 'meetups';
+Meetup.lookup = lookup;
+
+Weather.prototype = {
+  save: function(location_id) {
+    const SQL = `INSERT INTO ${this.tableName} (link, name, creation_date, host, location_id) VALUES($1, $2, $3, $4, $5)`;
+    const values = [this.link, this.name, this.creation_date, this.host, req.query.data.id];
+    client.query(SQL, values);
+  }
+}
+
+function getMeetups(request, response) {
+  const meetupsHandler = {
+    tableName: Meetup.tableName,
+    location: request.query.data.id,
+    cacheHit: function (result) {
+      response.send(result.rows);
+    },
+    cacheMiss: function () {
+      const url = `https://api.meetup.com/find/upcoming_events?sign=true&photo-host=public&lon=${req.query.data.longitude}&page=20&lat=${req.query.data.latitude}&key=${process.env.MEETUPS_API_KEY}`;
+      superagent.get(url)
+        .then(result => {
+          const meetupSummaries = result.body.events.map(upcomingMeetup => {
+            const summary = new Meetup(upcomingMeetup);
+            summary.save(request.query.data.id);
+            return summary;
+          });
+          response.send(meetupSummaries);
+        })
+        .catch(error => handleError(error, response));
+    }
+  };
+  Meetup.lookup(meetupsHandler);
+}
+
+//===============Trails==============================
+
+function Trail(nearbyTrail) {
+  this.tableName = 'trails';
+  this.name = nearbyTrail.name;
+  this.location = nearbyTrail.location;
+  this.length = nearbyTrail.length;
+  this.stars = nearbyTrail.stars;
+  this.star_votes = nearbyTrail.starVotes;
+  this.summary = nearbyTrail.summary;
+  this.trail_url = nearbyTrail.trail_url;
+  this.condition_details = nearbyTrail.conditionDetails;
+  this.condition_date = nearbyTrail.conditionDate.slice(0,9);
+  this.condition_time = nearbyTrail.conditionDate.slice(11,18);
+}
+
+Trail.tableName = 'trails';
+Trail.lookup = lookup;
+
+Trail.prototype = {
+  save: function(location_id) {
+    const SQL = `INSERT INTO ${this.tableName} (name, location, length, stars, star_votes, summary, trail_url, condition_details, condition_date, condition_time, location_id)
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+    const values = [this.name, this.location, this.length, this.stars, this.star_votes, this.summary, this.trail_url, this.condition_details, this.condition_date, this.condition_time, req.query.data.id];
+    client.query(SQL, values);
+  }
+}
+
+function getTrails(request, response) {
+  const trailHandler = {
+    tableName: Trail.talbeName,
+    location: request.query.data.id,
+    cacheHit: function (result) {
+      response.send(result.rows);
+    },
+    cacheMiss: function () {
+      const URL = `https://www.hikingproject.com/data/get-trails?lat=${req.query.data.latitude}&lon=${req.query.data.longitude}&key=${process.env.HIKING_PROJECT_API_KEY}`;
+      superagent.get(url)
+        .then(result => {
+          const trailSummaries = result.body.trails.map(localTrail => {
+            const summary = new Trail(localTrail);
+            summary.save(request.query.data.id);
+            return summary;
+          });
+          response.send(trailSummaries);
+        })
+        .catch(error => handleError(error, response));
+    }
+  };
+  Trail.lookup(trailHandler);
 }
