893b44a0106a02617479aafa120bc6ea69a2c17b
EvyHaan <evy.haan@gmail.com>
2018-12-20 16:29:39 -0800

    refactor complete;

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ function Location(query, res) {
   this.formatted_query = res.body.results[0].formatted_address;
   this.latitude = res.body.results[0].geometry.location.lat;
   this.longitude = res.body.results[0].geometry.location.lng;
+  this.short_name = res.body.results[0].address_components[0].short_name;
 }
 
 Location.lookupLocation = (location) => {
@@ -84,8 +85,8 @@ Location.lookupLocation = (location) => {
 
 Location.prototype = {
   save: function() {
-    const SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;`;
-    const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
+    const SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude, short_name) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING RETURNING id;`;
+    const values = [this.search_query, this.formatted_query, this.latitude, this.longitude, this.short_name];
     return client.query(SQL, values)
       .then(result => {
         this.id = result.rows[0].id;
@@ -102,7 +103,7 @@ function getLocation(request, response) {
       response.send(result.rows[0]);
     },
     cacheMiss: function() {
-      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.query}&key=${process.env.GEOCODE_API_KEY}`;
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.query}&key=${process.env.GOOGLE_MAPS_API}`;
       return superagent.get(url)
         .then(result => {
           const location = new Location(this.query, result);
@@ -141,7 +142,7 @@ function getWeather(request, response) {
       response.send(result.rows);
     },
     cacheMiss: function () {
-      const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${req.query.data.latitude},${req.query.data.longitude}`;
+      const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${request.query.data.latitude},${request.query.data.longitude}`;
       superagent.get(url)
         .then(result => {
           const weatherSummaries = result.body.daily.data.map(day => {
@@ -187,7 +188,7 @@ function getYelp(request, response) {
       response.send(result.rows);
     },
     cacheMiss: function() {
-      const URL = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${req.query.data.latitude}&longitude=${req.query.data.longitude}`;
+      const URL = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
       superagent.get(URL)
         .set('Authorization', `Bearer ${process.env.YELP_API}`)
         .then(result => {
@@ -222,7 +223,7 @@ Movie.lookup = lookup;
 Movie.prototype = {
   save: function(location_id) {
     const SQL = `INSERT INTO ${this.tableName} (title, overview, average_votes, total_votes, image_url, popularity, released_on, location_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)`;
-    const values = [this.title, this.overview, this.average_votes, this.total_votes, this.image_url, this.popularity, this.released_on, req.query.data.id];
+    const values = [this.title, this.overview, this.average_votes, this.total_votes, this.image_url, this.popularity, this.released_on, location_id];
     client.query(SQL, values);
   }
 }
@@ -235,7 +236,7 @@ function getMovies(request, response) {
       response.send(result.rows);
     },
     cacheMiss: function () {
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_DB_API}&query=${req.query.data.short_name}`;
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_DB_API}&query=${request.query.data.short_name}`;
       superagent.get(url)
         .then(result => {
           const movieSummaries = result.body.results.map(localMovie => {
@@ -260,13 +261,13 @@ function Meetup(upcomingMeetup) {
   this.host = upcomingMeetup.group.name;
 }
 
-Meetup.tablename = 'meetups';
+Meetup.tableName = 'meetups';
 Meetup.lookup = lookup;
 
-Weather.prototype = {
+Meetup.prototype = {
   save: function(location_id) {
     const SQL = `INSERT INTO ${this.tableName} (link, name, creation_date, host, location_id) VALUES($1, $2, $3, $4, $5)`;
-    const values = [this.link, this.name, this.creation_date, this.host, req.query.data.id];
+    const values = [this.link, this.name, this.creation_date, this.host, location_id];
     client.query(SQL, values);
   }
 }
@@ -279,7 +280,7 @@ function getMeetups(request, response) {
       response.send(result.rows);
     },
     cacheMiss: function () {
-      const url = `https://api.meetup.com/find/upcoming_events?sign=true&photo-host=public&lon=${req.query.data.longitude}&page=20&lat=${req.query.data.latitude}&key=${process.env.MEETUPS_API_KEY}`;
+      const url = `https://api.meetup.com/find/upcoming_events?sign=true&photo-host=public&lon=${request.query.data.longitude}&page=20&lat=${request.query.data.latitude}&key=${process.env.MEETUPS_API_KEY}`;
       superagent.get(url)
         .then(result => {
           const meetupSummaries = result.body.events.map(upcomingMeetup => {
@@ -307,7 +308,7 @@ function Trail(nearbyTrail) {
   this.summary = nearbyTrail.summary;
   this.trail_url = nearbyTrail.trail_url;
   this.condition_details = nearbyTrail.conditionDetails;
-  this.condition_date = nearbyTrail.conditionDate.slice(0,9);
+  this.condition_date = nearbyTrail.conditionDate.slice(0,10);
   this.condition_time = nearbyTrail.conditionDate.slice(11,18);
 }
 
@@ -318,20 +319,20 @@ Trail.prototype = {
   save: function(location_id) {
     const SQL = `INSERT INTO ${this.tableName} (name, location, length, stars, star_votes, summary, trail_url, condition_details, condition_date, condition_time, location_id)
     VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
-    const values = [this.name, this.location, this.length, this.stars, this.star_votes, this.summary, this.trail_url, this.condition_details, this.condition_date, this.condition_time, req.query.data.id];
+    const values = [this.name, this.location, this.length, this.stars, this.star_votes, this.summary, this.trail_url, this.condition_details, this.condition_date, this.condition_time, location_id];
     client.query(SQL, values);
   }
 }
 
 function getTrails(request, response) {
   const trailHandler = {
-    tableName: Trail.talbeName,
+    tableName: Trail.tableName,
     location: request.query.data.id,
     cacheHit: function (result) {
       response.send(result.rows);
     },
     cacheMiss: function () {
-      const URL = `https://www.hikingproject.com/data/get-trails?lat=${req.query.data.latitude}&lon=${req.query.data.longitude}&key=${process.env.HIKING_PROJECT_API_KEY}`;
+      const url = `https://www.hikingproject.com/data/get-trails?lat=${request.query.data.latitude}&lon=${request.query.data.longitude}&key=${process.env.HIKING_PROJECT_API_KEY}`;
       superagent.get(url)
         .then(result => {
           const trailSummaries = result.body.trails.map(localTrail => {
